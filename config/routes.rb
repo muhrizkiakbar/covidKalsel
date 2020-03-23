d6ca7e354bb7fd4188b4c0f0b192d13d7c3092c4
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-03-23 17:28:16 +0800

    add auth and many things

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   resources :hospitals
   resources :cities
   resources :permissions
-  resources :roles
+  
+  resources :roles do
+    resources :role_permissions
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
