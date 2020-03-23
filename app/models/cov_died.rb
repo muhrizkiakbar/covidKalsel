e864e094c1439d24eddb9d24711f9cae9768987a
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-03-23 15:22:08 +0800

    add friendly_id to cov died

--- app/models/cov_died.rb
+++ app/models/cov_died.rb
@@ -1,3 +1,9 @@
 class CovDied < ApplicationRecord
   belongs_to :city
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+
+  Paperclip.interpolates :slug do |attachment, style|
+      attachment.instance.slug
+  end
 end
