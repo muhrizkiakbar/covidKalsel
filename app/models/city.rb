b241c315afc513472400e0d95b455bd5ab08f0fd
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-03-23 15:19:53 +0800

    add friendly id to city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,10 @@
 class City < ApplicationRecord
+
+
+    extend FriendlyId
+    friendly_id :slug_candidates, use: :slugged
+
+    Paperclip.interpolates :slug do |attachment, style|
+        attachment.instance.slug
+    end
 end
