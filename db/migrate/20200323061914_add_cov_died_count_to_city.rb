ca2026591a51fd19e7d5bba65760199fcc72a0eb
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-03-23 18:15:13 +0800

    set default column value

--- db/migrate/20200323061914_add_cov_died_count_to_city.rb
+++ db/migrate/20200323061914_add_cov_died_count_to_city.rb
@@ -1,5 +1,5 @@
 class AddCovDiedCountToCity < ActiveRecord::Migration[5.2]
   def change
-    add_column :cities, :cov_died_count, :integer
+    add_column :cities, :cov_died_count, :integer, default: 0
   end
 end
