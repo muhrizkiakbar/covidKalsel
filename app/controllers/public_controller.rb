67b43c78a639bf47559bc62c7c40295fc3f97635
Nat4Lia <raztajr@gmail.com>
2020-04-06 06:19:03 +0800

    add status meninggal

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -10,6 +10,7 @@ class PublicController < ApplicationController
         @cov_positive_count = City.sum('cov_positive_count')
         @cov_odp_count = City.sum('cov_odp_count')
         @cov_pdp_count = City.sum('cov_pdp_count')
+        @cov_died_count = City.sum('cov_died_count')
 
         @city_last_updated = @cities.order("updated_at DESC").first
     end
