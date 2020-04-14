2568a4ebc290d4af8018c4e891234cec18fa54b3
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-04-14 18:48:17 +0800

    tes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -46,15 +46,19 @@ class DashboardController < ApplicationController
           @sum_positive = CovPositive.where('DATE(added_at) < ?',current_date_of_loop.to_s).sum(:amount)
           p "*" * 100
           p @sum_positive
-          result_data[0]["data"].push(Array.new([current_date_of_loop.to_s, @sum_positive])) 
           @sum_recovered = CovRecovered.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount) 
           result_data[1]["data"].push(Array.new([current_date_of_loop.to_s, @sum_recovered])) 
           @sum_died = CovDied.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount) 
           result_data[2]["data"].push(Array.new([current_date_of_loop.to_s, @sum_died])) 
+
+          result_data[0]["data"].push(Array.new([current_date_of_loop.to_s, @sum_positive-@sum_died-@sum_recovered])) 
+
           @sum_odp = CovOdp.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount) 
-          result_data[3]["data"].push(Array.new([current_date_of_loop.to_s, @sum_odp])) 
-          @sum_pdp = CovPdp.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount) 
-          result_data[4]["data"].push(Array.new([current_date_of_loop.to_s, @sum_pdp])) 
+          @sum_odp_processed = CovOdpProcessed.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount) 
+          result_data[3]["data"].push(Array.new([current_date_of_loop.to_s, @sum_odp-@sum_odp_processed])) 
+          @sum_pdp = CovPdp.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)
+          @sum_pdp_processed = CovPdpProcessed.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)  
+          result_data[4]["data"].push(Array.new([current_date_of_loop.to_s, @sum_pdp-@sum_pdp_processed])) 
       
           $i +=1;
           # puts "done"
