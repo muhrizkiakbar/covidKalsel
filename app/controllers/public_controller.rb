69761ba7e30e840bbccdcfc660c2f979f66b76ba
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-04-15 19:22:52 +0800

    tes

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -220,19 +220,19 @@ class PublicController < ApplicationController
           @sum_positive = CovPositive.where('DATE(added_at) <= ?',current_date_of_loop.to_s).sum(:amount)
           p "*" * 100
           p @sum_positive
-          @sum_recovered = CovRecovered.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)
+          @sum_recovered = CovRecovered.where('DATE(added_at) <= ?',current_date_of_loop.to_s).sum(:amount)
           result_data[1]["data"].push(Array.new([current_date_of_loop.to_s, @sum_recovered]))
-          @sum_died = CovDied.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)
+          @sum_died = CovDied.where('DATE(added_at) <= ?',current_date_of_loop.to_s).sum(:amount)
           result_data[2]["data"].push(Array.new([current_date_of_loop.to_s, @sum_died]))
 
-          result_data[0]["data"].push(Array.new([current_date_of_loop.to_s, @sum_positive-@sum_died-@sum_recovered]))
+          result_data[0]["data"].push(Array.new([current_date_of_loop.to_s, @sum_positive]))
 
           @sum_odp = CovOdp.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)
           @sum_odp_processed = CovOdpProcessed.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)
-          result_data[3]["data"].push(Array.new([current_date_of_loop.to_s, @sum_odp-@sum_odp_processed]))
+          result_data[3]["data"].push(Array.new([current_date_of_loop.to_s, @sum_odp))
           @sum_pdp = CovPdp.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)
           @sum_pdp_processed = CovPdpProcessed.where('DATE(added_at) <= ?',current_date_of_loop).sum(:amount)
-          result_data[4]["data"].push(Array.new([current_date_of_loop.to_s, @sum_pdp-@sum_pdp_processed]))
+          result_data[4]["data"].push(Array.new([current_date_of_loop.to_s, @sum_pdp]))
 
           $i +=1;
           # puts "done"
