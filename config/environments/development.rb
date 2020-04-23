a566dc818c5719e830510e22312d18e764c6a409
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-04-23 13:35:13 +0800

    tes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.telegram_updates_controller.session_store = :file_store,
-       Rails.root.join('tmp', 'session_store')
+  #config.telegram_updates_controller.session_store = :file_store,
+  #     Rails.root.join('tmp', 'session_store')
 end
