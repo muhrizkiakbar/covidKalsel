a1f23c171092888afa165ade48ad5d111103c099
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-04-24 19:49:27 +0800

    tes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   
-  routes.default_url_options = {host: 'https://corona.kalselprov.go.id', protocol: 'https'} # https is necessary!
+  routes.default_url_options = {host: 'corona.kalselprov.go.id', protocol: 'https'} # https is necessary!
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
