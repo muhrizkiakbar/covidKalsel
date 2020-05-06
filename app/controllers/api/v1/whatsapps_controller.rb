bd565b640d73d604838e63dc3be19304a2dd5e88
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-07 04:37:42 +0800

    tes

--- app/controllers/api/v1/whatsapps_controller.rb
+++ app/controllers/api/v1/whatsapps_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::WhatsappsController < ActionController::Base
         @cities = City.all
         @city_last_updated = @cities.order("updated_at DESC").first
         
-        return @text = { "message" => "Angka Sebaran di Kalimantan Selatan \nTotal Positif (Dirawat, Meninggal, Sembuh) : #{@cov_positive_total} \nDirawat : #{@cov_positive_count} \nSembuh : #{@cov_recovered_count} \nMeninggal : #{@cov_died_count} \nODP : #{@cov_odp_count} \nPDP : #{@cov_pdp_count }\n ."}
+        return @text = { "message" => "Angka Sebaran di Kalimantan Selatan \nTotal Positif (Dirawat, Meninggal, Sembuh) : #{@cov_positive_total} \nDirawat : #{@cov_positive_count} \nSembuh : #{@cov_recovered_count} \nMeninggal : #{@cov_died_count} \nODP : #{@cov_odp_count} \nPDP : #{@cov_pdp_count }\n"}
     end
 
     def rumah_sakit
